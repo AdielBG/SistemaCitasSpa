00544de984ea7e10743b9df8c63ce1464b98c75c
AdielBG <adielbg17.2@gmail.com>
2025-07-17 19:51:29 -0400

    Algunas validaciones de la Entidad Paciente

--- SistemaCitasSpa/SistemaCitasSpa/Models/Paciente.cs
+++ SistemaCitasSpa/SistemaCitasSpa/Models/Paciente.cs
@@ -1,20 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SistemaCitasSpa.Models;
 
 public partial class Paciente
 {
+    
     public int PacienteID { get; set; }
 
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
     public string Nombre { get; set; } = null!;
 
+    [Required(ErrorMessage = "El apellido es obligatorio.")]
     public string Apellido { get; set; } = null!;
 
+    [Phone(ErrorMessage = "Teléfono inválido.")]
     public string? Telefono { get; set; }
 
+    [EmailAddress(ErrorMessage = "Correo inválido.")]
     public string? Correo { get; set; }
 
+    [DataType(DataType.Date)]
     public DateOnly? FechaNacimiento { get; set; }
 
     public virtual ICollection<Citum> Cita { get; set; } = new List<Citum>();
