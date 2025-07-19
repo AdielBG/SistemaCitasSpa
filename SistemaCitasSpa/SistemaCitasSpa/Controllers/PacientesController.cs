ee4d4db6775c969f2875537e079b02a3a15e7c81
AdielBG <adielbg17.2@gmail.com>
2025-07-19 16:32:59 -0400

    Paciente - Index

--- SistemaCitasSpa/SistemaCitasSpa/Controllers/PacientesController.cs
+++ SistemaCitasSpa/SistemaCitasSpa/Controllers/PacientesController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Data.Entity;
+//using System.Linq;
+//using System.Net;
+//using System.Web.Mvc;
+using SistemaCitasSpa.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SistemaCitasSpa.Controllers
 {
     public class PacientesController : Controller
     {
-        public IActionResult Index()
+
+        private SpaDbContext db = new SpaDbContext();
+        
+        // GET: Pacientes
+        public ActionResult Index()
         {
-            return View();
+            try
+            {
+                var pacientes = db.Pacientes.ToList();
+                return View(pacientes);
+            }
+            catch (Exception ex)
+            {
+                // Maneja el error y lo muestra en una vista amigable o lo redirige
+                ViewBag.Error = "Ocurrió un error al cargar los pacientes: " + ex.Message;
+                return View(new List<Paciente>());
+            }
         }
     }
 }
