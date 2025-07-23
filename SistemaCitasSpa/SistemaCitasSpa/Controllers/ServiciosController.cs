dc0de280e67498a55acf42901d56bd952941e8be
AdielBG <adielbg17.2@gmail.com>
2025-07-23 11:56:26 -0400

    Servicios - Index

--- SistemaCitasSpa/SistemaCitasSpa/Controllers/ServiciosController.cs
+++ SistemaCitasSpa/SistemaCitasSpa/Controllers/ServiciosController.cs
@@ -1,13 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaCitasSpa.Models;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SistemaCitasSpa.Controllers
 {
     public class ServiciosController : Controller
     {
+        private readonly SpaDbContext _context;
 
-        public IActionResult Index()
+        public ServiciosController(SpaDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        // GET: Servicios
+        public async Task<IActionResult> Index(string mensaje = "")
+        {
+            ViewBag.Mensaje = mensaje;
+            return View(await _context.Servicios.ToListAsync());
+        }
+
+
+        // GET: ExportarCSV
+        public async Task<FileResult> ExportarCSV()
+        {
+            var servicios = await _context.Servicios.ToListAsync();
+
+            var builder = new StringBuilder();
+            builder.AppendLine("ID,Nombre,Descripción,Precio");
+
+            foreach (var serv in servicios)
+            {
+                builder.AppendLine($"{serv.ServicioID},{serv.NombreServicio},{serv.Descripcion},{serv.Precio}");
+            }
+
+            return File(Encoding.UTF8.GetBytes(builder.ToString()), "text/csv", "Servicios.csv");
+
+        }
+
         }
     }
-}
