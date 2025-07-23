064334e0f8e48468ab275cb376ae6dbe26adfd0f
AdielBG <adielbg17.2@gmail.com>
2025-07-23 11:40:26 -0400

    Servicios - ServiciosController

---
+++ SistemaCitasSpa/SistemaCitasSpa/Controllers/ServiciosController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaCitasSpa.Controllers
+{
+    public class ServiciosController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
