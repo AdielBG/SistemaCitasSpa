008b28b3fbd741444994cfaf7e7e5f2dc9cae59a
AdielBG <adielbg17.2@gmail.com>
2025-07-28 11:36:12 -0400

    Terapeuta - Validaciones del modelo

--- SistemaCitasSpa/SistemaCitasSpa/Models/Terapeutum.cs
+++ SistemaCitasSpa/SistemaCitasSpa/Models/Terapeutum.cs
@@ -1,15 +1,29 @@
-﻿namespace SistemaCitasSpa.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaCitasSpa.Models;
 
 public partial class Terapeutum
 {
     public int TerapeutaID { get; set; }
 
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [StringLength(100, ErrorMessage = "El nombre no puede tener más de 100 caracteres.")]
     public string Nombre { get; set; } = null!;
 
+    [Required(ErrorMessage = "La especialidad del terapeuta es obligatoria.")]
+    [StringLength(100, ErrorMessage = "La especialidad no puede tener más de 100 caracteres.")]
     public string? Especialidad { get; set; }
 
+
+    [Required(ErrorMessage = "El telefono del terapeuta es obligatorio.")]
+    [Phone(ErrorMessage = "El número de teléfono no es válido.")]
+    [StringLength(15, ErrorMessage = "El número de teléfono no puede tener más de 15 caracteres.")]
     public string? Telefono { get; set; }
 
+
+    [Required(ErrorMessage = "El correo del terapeuta es obligatorio.")]
+    [EmailAddress(ErrorMessage = "El correo electrónico no es válido.")]
+    [StringLength(100, ErrorMessage = "El correo electrónico no puede tener más de 100 caracteres.")]
     public string? Correo { get; set; }
 
     public virtual ICollection<Citum> Cita { get; set; } = new List<Citum>();
