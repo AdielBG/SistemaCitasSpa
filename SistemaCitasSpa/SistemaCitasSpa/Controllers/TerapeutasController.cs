d700546d79430c24ec9037cb6cc2021a0a49bc3b
AdielBG <adielbg17.2@gmail.com>
2025-07-28 11:52:47 -0400

    Terapeutas - Controller

---
+++ SistemaCitasSpa/SistemaCitasSpa/Controllers/TerapeutasController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaCitasSpa.Models;
+using System.Text;
+
+namespace SistemaCitasSpa.Controllers
+{
+    public class TerapeutasController : Controller
+    {
+        private readonly SpaDbContext _context;
+
+        public TerapeutasController(SpaDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
