298d68e35b41f4f0aa3aea96647421308b33cda3
AdielBG <adielbg17.2@gmail.com>
2025-07-30 12:30:58 -0400

    Validaciones del Modelo Citas

--- SistemaCitasSpa/SistemaCitasSpa/Models/Citum.cs
+++ SistemaCitasSpa/SistemaCitasSpa/Models/Citum.cs
@@ -1,17 +1,36 @@
-﻿namespace SistemaCitasSpa.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaCitasSpa.Models;
 
 public partial class Citum
 {
+    [Key]
     public int CitaID { get; set; }
 
+    [Required(ErrorMessage = "El campo Paciente es obligatorio.")]
+    [Display(Name = "Paciente")]
     public int PacienteID { get; set; }
 
+
+    [Required(ErrorMessage = "El campo Servicio es obligatorio.")]
+    [Display(Name = "Servicio")]
     public int ServicioID { get; set; }
 
+
+    [Required(ErrorMessage = "El campo Terapeuta es obligatorio.")]
+    [Display(Name = "Terapeuta")]
     public int TerapeutaID { get; set; }
 
+
+    [Required(ErrorMessage = "La fecha es obligatoria.")]
+    [DataType(DataType.Date)]
+    [Display(Name = "Fecha de la cita")]
     public DateOnly Fecha { get; set; }
 
+
+    [Required(ErrorMessage = "La hora es obligatoria.")]
+    [DataType(DataType.Time)]
+    [Display(Name = "Hora de la cita")]
     public TimeOnly Hora { get; set; }
 
     public virtual Paciente Paciente { get; set; } = null!;
